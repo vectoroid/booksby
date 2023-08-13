38806249377ce0fefc19392040107c7f0b218011
tm alone <tmalone@protonmail.ch>
2023-08-13 11:46:17 -0400

    Deleted template files -- no longer needed

--- booksby/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for booksby."""
-
-from booksby.cli import main  # pragma: no cover
-
-if __name__ == "__main__":  # pragma: no cover
-    main()
--- tests/test_base.py
+++
@@ -1,5 +0,0 @@
-from booksby.base import NAME
-
-
-def test_base():
-    assert NAME == "booksby"
